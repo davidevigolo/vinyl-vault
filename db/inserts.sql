3ee390a690e331907d7a7276a63dc53f36fcfc07
SalvaSiria3 <siria.salvalaio@gmail.com>
2026-01-19 13:04:13 +0100

    Piccola modifica al db (insert)

--- db/inserts.sql
+++ db/inserts.sql
@@ -957,7 +957,7 @@ INSERT INTO edition_track_part_of (disk_id, edition_name, track_id, track_number
 (8, 'Standard Edition', 82, 7), (8, 'Standard Edition', 83, 8), (8, 'Standard Edition', 84, 9),
 (8, 'Standard Edition', 85, 10), (8, 'Standard Edition', 86, 11),
 -- Hey Jude (The Beatles) - ID 9 - Tracks 87-88
-(9, 'Standard Edition', 87, 1), (9, 'Standard Edition', 88, 2),
+(9, 'Single', 87, 1), (9, 'Single', 88, 2),
 -- Bohemian Rhapsody (Queen) - ID 10 - Tracks 89-90
 (10, 'Single', 89, 1), (10, 'Single', 90, 2),
 -- Good Riddance (Gracie Abrams) - ID 11 - Tracks 373-384
@@ -1096,13 +1096,13 @@ INSERT INTO edition_track_part_of (disk_id, edition_name, track_id, track_number
 (35, 'Standard Edition', 352, 10), (35, 'Standard Edition', 353, 11), (35, 'Standard Edition', 354, 12),
 (35, 'Standard Edition', 355, 13), (35, 'Standard Edition', 356, 14),
 -- Blinding Lights (The Weeknd) - ID 36 - Tracks 357-357
-(36, 'Standard Edition', 357, 1),
+(36, 'Single', 357, 1),
 -- Save Your Tears (The Weeknd) - ID 37 - Tracks 358-358
-(37, 'Standard Edition', 358, 1),
+(37, 'Single', 358, 1),
 -- In the End (Linkin Park) - ID 38 - Tracks 359-360
-(38, 'Standard Edition', 359, 1), (38, 'Standard Edition', 360, 2),
+(38, 'Single', 359, 1), (38, 'Single', 360, 2),
 -- Sugar, We`re Goin Down (Fall Out Boy) - ID 39 - Tracks 361-362
-(39, 'Standard Edition', 361, 1), (39, 'Standard Edition', 362, 2),
+(39, 'Single', 361, 1), (39, 'Single', 362, 2),
 -- Certified Lover Boy (Drake) - ID 40 - Tracks 432-452
 (40, 'Standard Edition', 432, 1), (40, 'Standard Edition', 433, 2), (40, 'Standard Edition', 434, 3),
 (40, 'Standard Edition', 435, 4), (40, 'Standard Edition', 436, 5), (40, 'Standard Edition', 437, 6),
