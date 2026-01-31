e11cf5bfd8d0f8cbe1ff9be0eaf0867d74512925
Matteo Mantoan <mattemant@gmail.com>
2026-01-31 18:21:23 +0100

    Fix: change mandatory field message class in all forms

--- php/classes/DbConnection.php
+++ php/classes/DbConnection.php
@@ -3,39 +3,45 @@
 class DbConnection {
     private static $instance = null;
     private $connection;
-    
-    private $host = 'localhost';
-    private $database = 'dvigolo';
-    private $username = 'dvigolo';
-    private $password = 'oX2Uheib2phiequi';
-    
+
+    // private $host = 'localhost';
+    // private $database = 'dvigolo';
+    // private $username = 'dvigolo';
+    // private $password = 'oX2Uheib2phiequi';
+
+    private $host = 'tecweb_mysql';
+    private $database = 'tecweb_db';
+    private $username = 'tecweb_user';
+    private $password = 'tecweb_password';
+
     private function __construct() {
         try {
             $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-            
+
             if ($this->connection->connect_error) {
                 throw new Exception("Errore di connessione: " . $this->connection->connect_error);
             }
-            
+
             $this->connection->set_charset("utf8mb4");
         } catch (Exception $e) {
             die("Errore di connessione: " . $e->getMessage());
         }
     }
-    
+
     public static function get_instance() {
         if (self::$instance === null) {
             self::$instance = new DbConnection();
         }
         return self::$instance;
     }
-    
+
     public function get_connection() {
         return $this->connection;
     }
-    
-    private function __clone() {}
-    
+
+    private function __clone() {
+    }
+
     public function __wakeup() {
         throw new Exception("Impossibile deserializzare il singleton");
     }
